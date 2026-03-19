c57c3549f3db348f4ac24d3afba5f1137f218c16
Dean <runov.denis93@gmail.com>
2026-03-19 13:00:23 +0300

    Переделал возможность регистрации пользователей

--- auth.py
+++ auth.py
@@ -11,17 +11,18 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('username')
+        email = request.form.get('email')
         password = request.form.get('password')
         
-        user = User.query.filter_by(username=username).first()
+        # Ищем пользователя по email (не по username)
+        user = User.query.filter_by(email=email).first()
         
         if user and check_password_hash(user.password_hash, password):
             login_user(user)
             flash('Вы успешно вошли в систему')
             return redirect(url_for('index'))
         else:
-            flash('Неверное имя пользователя или пароль')
+            flash('Неверный email или пароль')
     
     return render_template('login.html')
 
@@ -40,12 +41,10 @@ def register():
         password = request.form.get('password')
         full_name = request.form.get('full_name')
         
-        user_exists = User.query.filter_by(username=username).first()
+        # Проверяем только уникальность email (username может повторяться)
         email_exists = User.query.filter_by(email=email).first()
         
-        if user_exists:
-            flash('Пользователь с таким именем уже существует')
-        elif email_exists:
+        if email_exists:
             flash('Email уже зарегистрирован')
         else:
             is_first = User.query.count() == 0
